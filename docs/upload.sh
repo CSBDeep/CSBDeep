9b7c447a118db75be61d90d3bb40d40646e2ae6c
Uwe Schmidt <uschmidt@mpi-cbg.de>
2018-04-13 21:24:58 +0200

    upload.sh should now work for everyone

--- docs/upload.sh
+++ docs/upload.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-OUTPUTDIR=/home/uschmidt/research/csbdeep/CSBDeep_code/docs/build/html
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+OUTPUTDIR="$DIR/build/html"
 
 # FTP_HOST=ftp.uweschmidt.org
 # FTP_USER=111766-27-uwe
@@ -10,4 +11,5 @@ OUTPUTDIR=/home/uschmidt/research/csbdeep/CSBDeep_code/docs/build/html
 FTP_HOST=home18440447.1and1-data.host
 FTP_USER=p6831244-fjug
 FTP_TARGET_DIR=/doc
+
 lftp sftp://$FTP_USER@$FTP_HOST -e "set sftp:auto-confirm yes ; mirror -R $OUTPUTDIR $FTP_TARGET_DIR ; quit"
