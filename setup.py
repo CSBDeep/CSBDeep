502b5b58d1250c6bf90ccd50a2cc1671e1496cbf
Uwe Schmidt <uschmidt@mpi-cbg.de>
2018-05-30 01:57:15 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,40 @@
 from __future__ import absolute_import
 from setuptools import setup, find_packages
 
+exec (open('csbdeep/version.py').read())
 
 setup(name='csbdeep',
-      version="0.1.0",
-      author='CSBDeep Team',
-      author_email='...',
+      version=__version__,
+      description='CSBDeep - a toolbox for Content-aware Image Restoration (CARE)',
+      url='https://github.com/csbdeep/csbdeep',
+      author='Uwe Schmidt, Martin Weigert',
+      author_email='uschmidt@mpi-cbg.de, mweigert@mpi-cbg.de',
       license='BSD 3-Clause License',
       packages=find_packages(),
 
-      install_requires=["keras>=2.0.7",
-                        "tqdm",
-                        "tifffile",
-                        "six",
-                        "pathlib2;python_version<'3'",
-                        "backports.tempfile;python_version<'3.4'",
-                        "numpy",
-                        "matplotlib",
-                        "scipy",
-                        # "tensorflow-gpu>=1.2.0",
-                        # "pandas>=0.20.1",
-                        ],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering',
+          'License :: OSI Approved :: BSD License',
+
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+
+      install_requires=[
+          "numpy",
+          "scipy",
+          "matplotlib",
+          "six",
+          "keras>=2.0.7",
+          "tifffile",
+          "tqdm",
+          "pathlib2;python_version<'3'",
+          "backports.tempfile;python_version<'3.4'",
+      ],
+
       entry_points={
           'console_scripts': [
               'care_predict = csbdeep.scripts.care_predict:main'
