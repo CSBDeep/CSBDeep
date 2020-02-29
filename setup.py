fc92e54a5a006cfc0a5ae860f16b5004308d413e
Uwe Schmidt <uschmidt@mpi-cbg.de>
2020-02-29 02:30:20 +0100

    Drop tensorflow 1.10 support/tests; add python 3.8 support

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(name='csbdeep',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
       ],
 
       install_requires=[
